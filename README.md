7c3aac83f624e4959942e9b65a7a98e7cc2200c6
harshh <harshh@gmao.com>
2022-05-15 23:04:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Steps
+* `npm install`
+* `truffle compile`
+* `truffle migrate --reset --network development`
+* `npm start` which should popup http://localhost:3000/ page
+
+## ```"Decentralized Photo Social Media."```
+
---
+++ truffle-config.js
@@ -0,0 +1,48 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKeys = process.env.PRIVATE_KEYS || ""
+const testnet = process.env.TESTNET;
+const mainnet = process.env.MAINNET;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 3
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(privateKeys.split(','), testnet),
+      network_id: 1666700000,
+      skipDryRun: true,
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(privateKeys.split(','), mainnet),
+      network_id: 1666600000,
+      skipDryRun: true,
+    }
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
